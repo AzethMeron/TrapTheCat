867eebf9a93d6304e1fb04f3caeb1fc768a44b8b
Azeth Meron <meronentertainment@gmail.com>
2024-04-14 00:38:12 +0200

    a

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         self.screen_width = 600
         self.screen_height = 600
         self.bg_color = (188, 158, 130)
-        self.board_size = 11
+        self.board_size = 13
         self.board_color = (255, 255, 255)
         self.trap_color = (155, 103, 60)
         self.cat_color = (0, 0, 255)
